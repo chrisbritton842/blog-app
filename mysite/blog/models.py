5837e71ffad0740b34f128c1a3945ef0c9138827
Chris Britton <chrisbritton790@gmail.com>
2022-11-16 14:50:03 -0800

    added custom manager, published

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,9 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+class PublishedManager(models.Manager):
+    def get_queryset(self):
+        return super().get_queryset().filter(status=Post.Status.PUBLISHED)
 class Post(models.Model):
     class Status(models.TextChoices):
         DRAFT = 'DF', 'Draft'
@@ -17,10 +20,9 @@ class Post(models.Model):
     status = models.CharField(max_length=2,
                             choices=Status.choices,
                             default=Status.DRAFT)
+    objects = models.Manager()
+    published = PublishedManager()
     class Meta:
         ordering = ['-publish']
-        indexes = [
-            models.Index(fields=['-publish']),
-        ]
     def __str__(self):
         return self.title
