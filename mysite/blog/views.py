d51cf7ef3132c7be1c0bca28e85b8b7759430b51
Chris Britton <chrisbritton790@gmail.com>
2022-11-16 18:12:05 -0800

    added file structure for templates

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.http import Http404
 from .models import Post
 
@@ -7,8 +7,5 @@ def post_list(request):
     return render(request, 'blog/post/list.html', {'posts': posts})
 
 def post_detail(request, id):
-    try:
-        post = Post.published.get(id=id)
-    except Post.DoesNotExist:
-        raise Http404("No Post found.")
+    post = get_object_or_404(Post, id=id, status=Post.Status.PUBLISHED)
     return render(request, 'blog/post/detail.html', {'post': post})
