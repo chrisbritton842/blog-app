05055aff1f3a4567d29d955de95e4735f510dfef
Chris Britton <chrisbritton790@gmail.com>
2022-11-17 16:28:51 -0800

    modified url pattern in blog/urls.py

--- mysite/blog/models.py
+++ mysite/blog/models.py
@@ -1,6 +1,7 @@
 from django.db import models
 from django.utils import timezone
 from django.contrib.auth.models import User
+from django.urls import reverse
 class PublishedManager(models.Manager):
     def get_queryset(self):
         return super().get_queryset().filter(status=Post.Status.PUBLISHED)
@@ -9,7 +10,7 @@ class Post(models.Model):
         DRAFT = 'DF', 'Draft'
         PUBLISHED = 'PB', 'Published'
     title = models.CharField(max_length=250)
-    slug = models.SlugField(max_length=250)
+    slug = models.SlugField(max_length=250, unique_for_date='publish')
     author = models.ForeignKey(User,
                             on_delete=models.CASCADE,
                             related_name='blog_posts')
@@ -24,5 +25,10 @@ class Post(models.Model):
     published = PublishedManager()
     class Meta:
         ordering = ['-publish']
+        indexes = [
+            models.Index(fields=['-publish']),
+        ]
     def __str__(self):
         return self.title
+    def get_absolute_url(self):
+        return reverse('blog:post_detail', args=[self.id])
