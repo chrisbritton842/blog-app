98f0dc3e4cb36876b48b8ac63d00a12798d84c19
Chris Britton <chrisbritton790@gmail.com>
2022-11-23 14:03:32 -0800

    modified post_detail view to display comments and comment form

--- mysite/blog/views.py
+++ mysite/blog/views.py
@@ -35,7 +35,13 @@ def post_detail(request, year, month, day, post):
                                 publish__year=year,
                                 publish__month=month,
                                 publish__day=day)
-    return render(request, 'blog/post/detail.html', {'post': post})
+    # List of active comments for this post
+    comments = post.comments.filter(active=True)
+    # Form for users to comment
+    form = CommentForm()
+    return render(request, 'blog/post/detail.html', {'post': post,
+                                                    'comments': comments,
+                                                    'form': form})
 
 def post_share(request, post_id):
     # Retrieve post by id
